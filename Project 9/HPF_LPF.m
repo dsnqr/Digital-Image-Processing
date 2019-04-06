e3884da1c4091499360d09ec8b3921bf8689ec2f
iAmDocBoom <soutrikmaiti.uta@gmail.com>
2019-04-05 22:40:11 -0500

    Final chnages

--- Project 9/HPF_LPF.m
+++ Project 9/HPF_LPF.m
@@ -2,242 +2,242 @@ clc;
 clear all;
 close all;
 
-%% Reading and displaying the 512*512 grayscale image
+%% Reading the image
 
 img = imread('lena512.bmp');
-figure;
-imshow(img);
-title('Orignal Image');
-saveas(gca,'original.jpg');
+[m1,n1]=size(img);
+figure();
+subplot(1,2,1);
+imshow(uint8(img));
+title('original image');
 
-%% DFT and display in frequency domain of the image read
-size = 512;
-DFT = fft2(img);
-DFT = fftshift(DFT);
-figure;
-imshow(DFT);
-title('DFT of Orignal Image')
-saveas(gca,'DFT.jpg');
+%% Performing the DFT on the image read
 
-%% Algorithm for the Low Pass Filter
+img =im2double(img);
+dft_img = fft2(img);
+subplot(1,2,2);
+imshow(log(1 + abs(fftshift(dft_img))));
+title('DFT of original image');
+saveas(gca,'origin_dft.jpg');
 
-LPF = zeros(size,size);
-D = fft2(img);
-D_0 = sqrt(40^2+60^2);
-for k=1:1:size
-for l=1:1:size
-D(k,l)=sqrt((k-(size/2))^2+(l-(size/2))^2);
-if (D(k,l)<=D_0)
-LPF(k,l)=1;
+%% Making the 2D mesh and plotting for the FFT of the image
+
+uu = 0:(m1-1);
+vv = 0:(n1-1);
+ii = find(uu > m1/2);
+uu(ii) = uu(ii)-m1;
+jj = find(vv > n1/2);
+vv(jj)=vv(jj)-n1;
+[kk,l]=meshgrid(vv,uu);
+DD=sqrt((kk.^2+l.^2));
+figure();
+mesh(real(fftshift(DD)));
+title('FFT of the original image given by D(u,v)');
+saveas(gca,'mesh.jpg');
+
+ 
+
+img_hpf  = HPF_img(30,DD);
+img_g_lpf = g_LPF_img(16,DD);
+img_g_hpf = g_HPF_img(16,DD);
+
+img_b_lpf = b_LPF_img(50,3,DD);
+img_b_hpf= b_HPF_img(50,3,DD);
+
+
+
+%% Applying LPF to image and plotting results
+
+img_lpf  = LPF_img(40,DD);
+img_dft_LPF = img_lpf.*dft_img;
+ifft_img_LPF = real(ifft2(img_dft_LPF));
+figure();
+subplot(1,2,1);
+imshow(log(1+abs(fftshift(img_dft_LPF))),[]);
+title('LPF frequency response of image');
+subplot(1,2,2);
+imshow(ifft_img_LPF,[]);
+title('LPF filtered Image');
+saveas(gca,'LPF_img.jpg');
+
+%% Applying HPF to image and plotting results
+
+img_dft_HPF = img_hpf.*dft_img;
+ifft_img_HPF = real(ifft2(img_dft_HPF));
+figure();
+subplot(1,2,1);
+imshow(log(1+abs(fftshift(img_dft_HPF))),[]);
+title('HPF frequency response of image');
+subplot(1,2,2);
+imshow(ifft_img_HPF,[]);
+title('HPF filtered image');
+saveas(gca,'HPF_img.jpg');
+
+%% Applying Gaussian LPF to image and plotting the results
+
+img_dft_g_LPF = img_g_lpf.*dft_img;
+ifft_img_g_LPF = real(ifft2(img_dft_g_LPF));
+figure();
+subplot(1,2,1);
+imshow(log(1+abs(fftshift(img_dft_g_LPF))),[]);
+title('GAUSSIAN LOW PASS filtered frequency response of image');
+subplot(1,2,2);
+imshow(ifft_img_g_LPF,[]);
+title('GAUSSIAN LOW PASS filtered image');
+saveas(gca,'g_LPF_img.jpg');
+
+
+%% Applying Gaussian LPF to image and plotting the results
+
+img_dft_g_HPF = img_g_hpf.*dft_img;
+ifft_img_g_HPF = real(ifft2(img_dft_g_HPF));
+figure();
+subplot(1,2,1);
+imshow(log(1+abs(fftshift(img_dft_g_HPF))),[]);
+title('GAUSSIAN HIGH PASS filtered frequency response image');
+subplot(1,2,2);
+imshow(ifft_img_g_HPF,[]);
+title('GAUSSIAN HIGHPASS filtered image');
+saveas(gca,'g_HPF_img.jpg');
+
+
+%% Applying Butterworth LPF to image and plotting the results
+
+img_dft_b_LPF = img_b_lpf.*dft_img;
+ifft_img_b_LPF = real(ifft2(img_dft_b_LPF));
+figure();
+subplot(1,2,1);
+imshow(log(1+abs(fftshift(img_dft_b_LPF))),[]);
+title('BUTTERWORTH LOW PASS filtered frequency response image');
+subplot(1,2,2);
+imshow(ifft_img_b_LPF,[]);
+title('BUTTERWORTH LOW PASS filtered image');
+saveas(gca,'b_LPF_img.jpg');
+
+%% Applying Butterworth HPF to image and plotting the results
+
+img_dft_b_HPF = img_b_hpf.*dft_img;
+ifft_img_b_HPF = real(ifft2(img_dft_b_HPF));
+figure();
+subplot(1,2,1);
+imshow(log(1+abs(fftshift(img_dft_b_HPF))),[]);
+title('BUTTERWORTH HIGH PASS filtered frequency response');
+subplot(1,2,2);
+imshow(ifft_img_b_HPF,[]);
+title('BUTTERWORTH HIGH PASS filtered image');
+saveas(gca,'b_HPF_img.jpg');
+
+
+
+%% Algorithm for Low Pass Filter
+
+function LPF = LPF_img(d_0,d_d)
+mm=512;
+nn=mm;
+for uu = 1:mm
+    for vv = 1:nn
+        if(d_d(uu,vv) <= d_0)
+            LPF(uu,vv) = 1;
+        else
+            LPF(uu,vv) = 0;
+        end
+    end
 end
-end
-end
-
-%% Display the 3D plot of the LPF
-figure
-mesh(LPF);
-xlabel('Spatial freq l');
-ylabel('Spatial freq k');
-zlabel('Amplitude ');
-colormap(hsv);
-shading interp;
-alpha(0.7);
-grid on;
-axis tight;
+%% 3D plot of ideal LPF
+figure();
+mesh(fftshift(LPF)),
+title('Low Pass Filter 3D');
 saveas(gca,'LPF_3D.jpg');
-hold on;
+end
 
-%% Plotting the results of LPF on the image
+%% Algorithm for High Pass Filter
 
-figure
-imshow(LPF);
-title('LP Filter');
-saveas(gca,'LPF.jpg');
+function HPF = HPF_img(d_0,d_d)
+mm=512;nn=mm;
+for uu = 1:mm
+    for vv = 1:nn
+        if(d_d(uu,vv)<=d_0)
+            HPF(uu,vv) = 0;
+        else
+            HPF(uu,vv) = 1;
+        end
+    end
+end
+%% 3D plot of ideal HPF
 
-figure
-f_img = ifft2(LPF);
-imshow(f_img);
-title('f_img');
+figure();
+mesh(fftshift(HPF)),
+title('High Pass Filter 3D');
+saveas(gca,'HPF_3D.jpg');
+end
 
-% %% Algorithm for High Pass Filter
-% 
-% size=512;
-% HPF = zeros(size,size);
-% d = zeros(size,size);
-% D_0 = sqrt(40^2+60^2);
-% for k=1:1:size
-% for l=1:1:size
-% d(k,l) = sqrt((k-(size/2))^2+(l-(size/2))^2);
-% if (d(k,l)>=D_0)
-% HPF(k,l) = 1;
-% end
-% end
-% end
-% 
-% %% Plotting the results of HPF on the image
-% 
-% figure
-% imshow(HPF);
-% title('HP Filter');
-% saveas(gca,'HPF_img.jpg');
-% 
-% %% Display the 3D plot of the HPF
-% figure
-% mesh(HPF);
-% xlabel('Spatial freq l');
-% ylabel('Spatial freq k');
-% zlabel('Amplitude g');
-% colormap(hsv);
-% shading interp;alpha(0.7);
-% grid on;
-% axis tight;
-% hold on;
-% saveas(gca,'HPF_3D.jpg');
-% 
-% %% Algorithm for Gaussian Low Pass Filter
-% 
-% sigma = 16;
-% gaussian_LPF = zeros(512,512);
-% for k=1:1:size
-% for l=1:1:size
-% gaussian_LPF(k,l)=exp(-((k-(size/2))^2+(l-(size/2))^2)/(2*sigma^2));
-% end
-% end
-% 
-% %% Display the 3D plot of the Gaussian LPF
-% 
-% figure
-% surf(gaussian_LPF, 'EdgeColor', 'none');
-% xlabel('Spatial freq l');
-% ylabel('Spatial freq k');
-% zlabel('Amplitude g');
-% colormap(hsv);
-% shading interp;
-% alpha(0.7);
-% grid on;
-% axis tight;
-% hold on;
-% saveas(gca,'Gaussian_LPF_3D.jpg');
-% 
-% %% Display Gaussian LPF
-% 
-% figure
-% imshow(gaussian_LPF);
-% title('Gaussian LPF')
-% saveas(gca,'G_LPF.jpg');
-% gaussianLP_transform = fft2(gaussian_LPF);
-% gaussianLP_transform = fftshift(gaussianLP_transform);
-% 
-% %% Plotting the results of Gaussian LPF on the image
-% 
-% figure
-% imshow(gaussianLP_transform);
-% title('Gaussian LPF transform');
-% saveas(gca,'G_LPF_img.jpg');
-% 
-% %% Algorithm for Gaussian High Pass Filter
-% sigma = 16;
-% gaussian_HPF = zeros(512,512);
-% for k=1:1:size
-% for l=1:1:size
-% gaussian_HPF(k,l) = 1-exp(-((k-(size/2))^2+(l-(size/2))^2)/(2*sigma^2));
-% end
-% end
-% 
-% %% Display 3D plot of Gaussian HPF
-% 
-% figure
-% surf(gaussian_HPF, 'EdgeColor', 'none');
-% xlabel('Spatial freq l');
-% ylabel('Spatial freq k');
-% zlabel('Amplitude g');
-% colormap(hsv);
-% shading interp;
-% alpha(0.7);
-% grid on;
-% axis tight;
-% hold on;
-% saveas(gca,'Gaussian_HPF_3D.jpg');
-% 
-% %% Dispaly Gaussian HPF
-% 
-% figure
-% imshow(gaussian_HPF);
-% title('Gaussian HP');
-% saveas(gca,'G_HPF.jpg');
-% gaussianHP_transform = fft2(gaussian_HPF);
-% gaussianHP_transform = fftshift(gaussianHP_transform);
-% 
-% %% Plotting the results of Gaussian HPF on the image
-% 
-% figure
-% imshow(gaussianHP_transform);
-% title('Gaussian HP transform');
-% saveas(gca,'G_HPF_img.jpg');
-% 
-% 
-% %% Algorithm for Butterworth Low Pass Filter
-% 
-% Butterworth_LPF = zeros(512,512);
-% n = 3;
-% D_0 = 40;
-% for k=1:1:size
-% for l=1:1:size
-% d(k,l) = sqrt((k-(size/2))^2+(l-(size/2))^2);
-% Butterworth_LPF(k,l) = 1/(1+(d(k,l)/D_0)^(2*n));
-% end
-% end
-% 
-% %% Display Butterworth LPF
-% 
-% figure
-% imshow(Butterworth_LPF);
-% title('Butterworth LPF');
-% saveas(gca,'Butterworth_LPF.jpg');
-% 
-% %% Display 3D plot of Butterworth LPF
-% 
-% figure
-% surf(Butterworth_LPF, 'EdgeColor', 'none');
-% xlabel('Spatial freq l');
-% ylabel('Spatial freq k');
-% zlabel('Amplitude g');
-% colormap(hsv);
-% shading interp;
-% alpha(0.7);
-% grid on;
-% axis tight;
-% hold on;
-% saveas(gca, 'Butterworth_LPF_3D.jpg')
-% 
-% %% Algorithm for Butterworth High Pass Filter
-% Butterworth_HPF = zeros(512,512);
-% n=3;
-% d0=50;
-% for k=1:1:size
-% for l=1:1:size
-% d(k,l)=sqrt((k-(size/2))^2+(l-(size/2))^2);
-% Butterworth_HPF(k,l)=1/(1+(d0/d(k,l))^(2*n));
-% end
-% end
-% 
-% %% Displaying Butterworth HPF
-% 
-% figure
-% imshow(Butterworth_HPF);
-% title('Butterworth HP');
-% saveas(gca,'Butterworth_HPF.jpg');
-% %% Display 3D plot of Butterworth HPF
-% 
-% figure
-% surf(Butterworth_HPF, 'EdgeColor', 'none');
-% xlabel('Spatial freq l');
-% ylabel('Spatial freq k');
-% zlabel('Amplitude g');
-% colormap(hsv);
-% shading interp;
-% alpha(0.7);
-% grid on;
-% axis tight;
-% hold on;
-% saveas(gca, 'Butterworth_HPF_3D.jpg');
+%% Algorithm for Gaussian Low Pass Filter
+
+function g_LPF = g_LPF_img(sgma,d_d)
+mm=512;nn=mm;
+for uu = 1:mm
+    for vv = 1:nn
+        g_LPF(uu,vv) = exp(-1*(d_d(uu,vv)^2)/(2*sgma^2));
+    end
+end
+%% 3D plot of Gaussian Low Pass Filter
+
+figure();
+mesh(fftshift(g_LPF)),
+title('Gaussian Low Pass Filter 3D');
+saveas(gca,'g_LPF_3D.jpg');
+end
+
+
+%% Algorithm for Gaussian High Pass Filter
+
+function g_HPF = g_HPF_img(sgma,d_d)
+mm=512;nn=mm;
+for uu = 1:mm
+    for vv = 1:nn
+        g_HPF(uu,vv) = 1-exp(-1*(d_d(uu,vv)^2)/(2*sgma^2));
+    end
+end
+%% 3D plot of Gaussian High Pass Filter
+
+figure();
+mesh(fftshift(g_HPF)),
+title('Gaussian HPF');
+saveas(gca,'g_HPF_3D.jpg');
+end
+
+%% Algorithm for Butterworth Low Pass Filter
+
+function b_LPF = b_LPF_img(d_0,o,d_d)
+mm=512;nn=mm;
+for uu=1:mm
+    for vv=1:nn
+        b_LPF(uu,vv) = 1/(1+(d_d(uu,vv)/d_0)^(2*o));
+    end
+end
+%% 3D plot of Butterworth Low Pass Filter
+figure();
+mesh(fftshift(b_LPF)),
+title('Butterworth Low Pass Filter');
+saveas(gca,'b_LPF_3D.jpg');
+end
+ 
+
+%% Algorithm for Butterworth High Pass Filter
+
+function b_HPF = b_HPF_img(d_0,o,d_d)
+mm=512;nn=mm;
+for uu = 1:mm
+    for vv = 1:nn
+        b_HPF(uu,vv) = 1/(1+(d_0/d_d(uu,vv))^(2*o));
+    end
+end
+%% 3D plot of Butterworth High Pass Filter
+
+figure();
+mesh(fftshift(b_HPF)),
+title('Butterworth High Pass Filter');
+saveas(gca,'b_HPF_3D.jpg');
+end
+ 
+
